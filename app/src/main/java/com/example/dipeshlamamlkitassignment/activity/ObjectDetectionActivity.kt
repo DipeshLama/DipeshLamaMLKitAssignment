c81ae0436f7017c1b541cfbc5afd6d95709b73a9
DipeshLama <dipeslama32@gmail.com>
2022-07-13 16:46:27 +0545

    Changes to code

--- app/src/main/java/com/example/dipeshlamamlkitassignment/activity/ObjectDetectionActivity.kt
+++ app/src/main/java/com/example/dipeshlamamlkitassignment/activity/ObjectDetectionActivity.kt
@@ -2,6 +2,7 @@ package com.example.dipeshlamamlkitassignment.activity
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -10,6 +11,9 @@ import androidx.activity.result.contract.ActivityResultContracts
 import com.example.dipeshlamamlkitassignment.R
 import com.example.dipeshlamamlkitassignment.constants.BundleConstants
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabel
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import com.google.mlkit.vision.objects.DetectedObject
 import com.google.mlkit.vision.objects.ObjectDetection
 import com.google.mlkit.vision.objects.defaults.ObjectDetectorOptions
@@ -23,7 +27,29 @@ class ObjectDetectionActivity : AppCompatActivity() {
         btnCamOpenObjDetection.setOnClickListener {
             openCamera()
         }
+        btnImageLabeling.setOnClickListener {
+            imageLabeling()
+        }
     }
+
+    private fun imageLabeling() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        launcher.launch(intent)
+    }
+
+    private val launcher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            when (result.resultCode) {
+                RESULT_OK -> {
+                    val extra = result.data?.extras
+                    val bitmap = extra?.get(BundleConstants.data) as Bitmap?
+                    if (bitmap != null) {
+                        detectImageLabeling(bitmap)
+                    }
+                }
+                RESULT_CANCELED -> return@registerForActivityResult
+            }
+        }
 
     private fun openCamera() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
@@ -46,6 +72,24 @@ class ObjectDetectionActivity : AppCompatActivity() {
                 }
             }
         }
+
+    private fun detectImageLabeling(bitmap: Bitmap) {
+        val image = InputImage.fromBitmap(bitmap, 0)
+        val labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
+        labeler.process(image)
+            .addOnSuccessListener {
+                processImageLabeling(it)
+
+            }
+            .addOnFailureListener {
+                Toast.makeText(this, it.message, Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun processImageLabeling(labels: List<ImageLabel>) {
+        txvObjectText.text = labels[0].text
+    }
+
 
     private fun detectImage(bitmap: Bitmap) {
         val options = ObjectDetectorOptions.Builder()
